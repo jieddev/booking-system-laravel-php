4d23a7a6adcf7f20dd7a51f88d38f3bc3a052faa
Jiedel Ruizo <jiedelflorentino.ruizo@gmail.com>
2026-09-18 22:59:14 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }} ">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Certicode Meeting Scheduling Booking</title>
+</head>
+<body>
+  <p>Test</p>
+</body>
+</html>
